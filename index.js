87b3e36b937b70de546063f55b42027aa982931d
Noah Salmon <noahsalmon98@gmail.com>
2023-09-12 21:21:06 -0400

    write function to generate logo using input data

--- index.js
+++ index.js
@@ -25,3 +25,31 @@ const questions = [
     }
 ]
 
+function generateLogo(text, textColor, shape, shapeColor) {
+    let shapeEl;
+  
+    if (shape === 'circle') {
+      shapeEln = `<circle cx="50%" cy="50%" r="50%" fill="${shapeColor}" />`;
+    } else if (shape === 'triangle') {
+      shapeEl = `<polygon points="0,0 100,100 0,100" fill="${shapeColor}" />`;
+    } else if (shape === 'square') {
+      shapeEl = `<rect width="100" height="100" fill="${shapeColor}" />`;
+    } else {
+      throw new Error('Invalid shape');
+    }
+    const svgMarkup = `
+      <svg width="300" height="200">
+        ${shapeEl}
+        <text x="50%" y="50%" fill="${textColor}" text-anchor="middle" dominant-baseline="middle" font-size="48">${text}</text>
+      </svg>
+    `;
+  
+    const filePath = path.join('examples', 'logo.svg');
+    fs.writeFileSync(filePath, svgMarkup);
+    console.log(`Generated ${filePath}`);
+  }
+  
+  module.exports = {
+    promptUser,
+    generateLogo
+  };
