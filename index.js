d35fe53ccac5412798a024f5702daff261afb3b9
Noah Salmon <noahsalmon98@gmail.com>
2023-09-12 21:15:40 -0400

    added prompts to index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+const questions = [
+    {
+        type: "input",
+        name: "text",
+        message: "Enter text up to 3 characters for your logo.",
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter the text color (keyword or hexadecimal number).",
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape for the logo.",
+        choices: ["Circle", "Triangle", "Square"],
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color for the shape of your logo."
+    }
+]
+
